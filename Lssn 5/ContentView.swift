cee7497dc353763aacb0ca82e6e83f34bbedb833
alexanderivv <thealexashka@gmail.com>
2023-03-02 14:34:48 +0300

    Initial Commit

---
+++ Lssn 5/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Lssn 5
+//
+//  Created by Alex Ander on 2.03.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lssn 5/Lssn_5App.swift
@@ -0,0 +1,17 @@
+//
+//  Lssn_5App.swift
+//  Lssn 5
+//
+//  Created by Alex Ander on 2.03.23.
+//
+
+import SwiftUI
+
+@main
+struct Lssn_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
